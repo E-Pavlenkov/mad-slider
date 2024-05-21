f38c3afe51dc7c6266bc5a33bec002e97dc627c2
E-Pavlenkov <mad21@mail.ru>
2024-05-21 15:32:08 +0300

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mad-slider
 
-Mad Slider is very light (js - 3,5kb + css - 1,3kb = 4,8kb), simple, responsive and touch-enabled.
+Mad Slider is very light (js - 4,88kb + css - 1,42kb = 6,3kb), simple, responsive and touch-enabled.
 Simple to setup. 
 Include JS and CSS files and write JSON setting in data-config.
 See example.html for code.
@@ -15,3 +15,19 @@ Main features:
 * CSS-only navigation controls (no graphics) for easy customization
 * any content support
 * JavaScript breakpoints 
+
+
+All settings:
+* slidesPerView
+* slideMinWidth
+* autoplay
+* autoheight
+* loop
+* pagination
+* counter
+* breakpoints
+* windowbreakpoints
+* arrows
+* prev
+* next
+
--- js/mad-slider.js
+++ js/mad-slider.js
@@ -1,12 +1,12 @@
-/* mad slider */
+/* mad slider 2024-05-21 */
 document.addEventListener("DOMContentLoaded",function() {
     var sliders = document.querySelectorAll('.mad-slider');
     if (sliders) {
-        sliders.forEach(function(item) {madSlider(item)});
+        sliders.forEach(function(item) { madSlider(item)} );
     }
 });
 
-function madSlider(wrapper) {
+function madSlider(wrapper, goto = 0) {
     var params = wrapper.dataset.config ? JSON.parse(wrapper.dataset.config) : {};
 
     var slidesPerView = params.slidesPerView ? params.slidesPerView : 1,
@@ -14,32 +14,38 @@ function madSlider(wrapper) {
         autoplay = params.autoplay ? params.autoplay : 0, playid,
         counter = false,
         loop = (params.loop == undefined ? true : params.loop),
-        pagination = params.pagination ? (params.pagination == true ? wrapper.querySelector('.mad-slider-pagination') : document.querySelector(params.pagination)) : NaN,
-        breakpoints = params.breakpoints ? params.breakpoints : NaN;
+        pagination = params.pagination ? (params.pagination == true ? wrapper.querySelector('.mad-slider-pagination') : document.querySelector(params.pagination)) : NaN, paginationItems,
+        breakpoints = params.breakpoints ? params.breakpoints : NaN,
+        windowbreakpoints = params.windowbreakpoints ? params.windowbreakpoints : NaN;
 
     if (params.arrows) {
-        var prev = params.prev ? document.querySelector(params.prev) : wrapper.querySelector('.mad-slider-prev');
-        var next = params.next ? document.querySelector(params.next) : wrapper.querySelector('.mad-slider-next');
+        if (params.prev) document.querySelector(params.prev).addEventListener('click', function () { clearInterval(playid); shiftSlide(-1) });
+        if (params.next) document.querySelector(params.next).addEventListener('click', function () { clearInterval(playid); shiftSlide(1) });
+        if (wrapper.querySelector('.mad-slider-prev')) {
+            wrapper.querySelector('.mad-slider-prev').addEventListener('click', function () { clearInterval(playid); shiftSlide(-1) });
+        }
+        if (wrapper.querySelector('.mad-slider-next')) {
+            wrapper.querySelector('.mad-slider-next').addEventListener('click', function () { clearInterval(playid); shiftSlide(1) });
+        }
 
         counter = (params.counter == true ? wrapper.querySelector('.mad-slider-counter') : document.querySelector(params.counter));
-
-        if (prev) prev.addEventListener('click', function () { clearInterval(playid); shiftSlide(-1) });
-        if (next) next.addEventListener('click', function () { clearInterval(playid); shiftSlide(1) });
     }
 
     var items = wrapper.querySelector('.mad-slides');
+    var gap = parseFloat(window.getComputedStyle(items, null).getPropertyValue("gap")); 
+    if (!gap) gap = 0;
 
-    var posX1 = 0, posX2 = 0, posInitial, posFinal, i,
+    var posX1 = 0, posX2 = 0, posInitial, posFinal, i, posY1 = 0, posY2 = 0, noshift = false,
         slides = items.getElementsByClassName('mad-slide'),
         slidesLength = slides.length,
         slidesShift = loop ? slides.length : 0,
         slideSize = wrapper.offsetWidth / slidesPerView,
-        threshold = slideSize / 3,
+        threshold = slideSize / 10,
         index = 0,
         allowShift = true,
-        autoheight = params.height && params.height == 'auto' && slidesPerView == 1 ? true : false;
+        autoheight = params.height && params.height == 'auto';
 
-    if (counter) counter.innerHTML = "1 / " + slidesLength;    
+    if (counter) counter.innerHTML = (goto + 1) + " / " + slidesLength;    
 
     if (autoheight) {
         for (i = 0; i < slidesLength; i++) {
@@ -72,21 +78,34 @@ function madSlider(wrapper) {
 
     /* pagination */
     if (pagination) {
-        for (i = 0; i < slidesLength; i++) {
-            var button = document.createElement("span");
-            button.classList.add('mad-slider-button');
-            if (i == 0) button.classList.add('active');
-
-            button.dataset.index = i;
-            button.addEventListener('click', function () {
-                gotoSlide(this.dataset.index);
-
-                Array.from(pagination.getElementsByClassName('mad-slider-button')).forEach(element => {
-                    element.classList.remove('active');
+        if (pagination.innerHTML == '') {
+            for (i = 0; i < slidesLength; i++) {
+                var button = document.createElement("span");
+                button.classList.add('mad-slider-button');
+                if (i == 0) button.classList.add('active');
+    
+                button.dataset.index = i;
+                button.addEventListener('click', function () {
+                    gotoSlide(this.dataset.index);
                 });
-                this.classList.add('active');
-            });
-            pagination.appendChild(button);
+                pagination.appendChild(button);
+            }
+
+            paginationItems = Array.from(pagination.getElementsByClassName('mad-slider-button'));
+
+        } else {
+            for (const child of pagination.children) {
+                child.addEventListener('click', function () {
+                    gotoSlide(this.dataset.index);
+    
+                    Array.from(pagination.children).forEach(element => {
+                        element.classList.remove('active');
+                    });
+                    this.classList.add('active');
+                });
+            }
+
+            paginationItems = Array.from(pagination.children);
         }
     }
 
@@ -94,9 +113,9 @@ function madSlider(wrapper) {
     items.onmousedown = dragStart;
     items.onmousedup = dragEnd;
     /* Touch events */
-    items.addEventListener('touchstart', dragStart, { passive: true });
-    items.addEventListener('touchend', dragEnd, { passive: true });
-    items.addEventListener('touchmove', dragAction, { passive: true });
+    items.addEventListener('touchstart', dragStart, { passive: false });
+    items.addEventListener('touchend', dragEnd, { passive: false });
+    items.addEventListener('touchmove', dragAction, { passive: false, capture: true });
     /* Transition events */
     items.addEventListener('transitionend', checkIndex);
 
@@ -108,28 +127,51 @@ function madSlider(wrapper) {
 
         if (breakpoints) {
             for (var [key, value] of Object.entries(breakpoints)) {
-                if (wrapper.offsetWidth < key) {
+                if (wrapper.offsetWidth < parseInt(key)) {
                     slidesPerView = value;
                     break;
                 }
             }
         }
 
-        slideSize = wrapper.offsetWidth / slidesPerView;
-        threshold = slideSize / 3;
+        if (windowbreakpoints) {
+            for (var [key, value] of Object.entries(windowbreakpoints)) {
+                if (document.body.clientWidth < parseInt(key)) {
+                    slidesPerView = value;
+                    break;
+                }
+            }
+        }
+
+        gap = parseFloat(window.getComputedStyle(items, null).getPropertyValue("gap")); 
+        if (!gap) gap = 0;
+
+        slideSize = wrapper.offsetWidth / slidesPerView - (gap * (slidesPerView - 1) / (slidesPerView));
+        
+        threshold = slideSize / 5;
         Array.from(slides).forEach(element => {
             element.style.width = slideSize + "px";
-            items.style.left = -((index + slidesShift) * slideSize) + "px";
         });
 
-        if (autoheight) { 
+        items.style.left = -((index + slidesShift) * (slideSize + gap)) + "px";
+
+        if (autoheight && slides.length) { 
             var h = slides[index].clientHeight;
 
             for (i = 1; i < slidesPerView; i++) {
-                h = slides[index + i].clientHeight > h ? slides[index + i].clientHeight : h;
+                if (h < slides[index + i].clientHeight) h = slides[index + i].clientHeight;
             }
             items.style.height = h + "px";
         }
+
+
+    }
+
+    if (goto) {
+        n = parseInt(goto);
+
+        items.style.left = (-((slideSize + gap) * (n + slidesShift))) + "px";
+        index = n;
     }
 
     onResize();
@@ -137,18 +179,23 @@ function madSlider(wrapper) {
     new ResizeObserver(onResize).observe(wrapper);
 
     function dragStart(e) {
+        if (!allowShift) return;
+
+        items.classList.add('dragging');
+        
         clearInterval(playid);
         e = e || window.event;
         /* for mouse only */
         if (!e.touches) e.preventDefault();
-        if (!allowShift) return;
 
         posInitial = items.offsetLeft;
 
         if (e.type == 'touchstart') {
             posX1 = e.touches[0].clientX;
+            posY1 = e.touches[0].clientY;
         } else {
             posX1 = e.clientX;
+            posY1 = e.clientY;
             document.onmouseup = dragEnd;
             document.onmousemove = dragAction;
         }
@@ -157,14 +204,40 @@ function madSlider(wrapper) {
     function dragAction(e) {
         e = e || window.event;
 
+        if (noshift) return false;
+
         if (e.type == 'touchmove') {
             posX2 = posX1 - e.touches[0].clientX;
-            posX1 = e.touches[0].clientX;
+            posY2 = posY1 - e.touches[0].clientY;
+            
         } else {
             posX2 = posX1 - e.clientX;
-            posX1 = e.clientX;
+
+            var is = items.querySelectorAll('a');
+            is.forEach(function(i) {
+                i.style.pointerEvents = 'none';
+            });
         }
-        items.style.left = (items.offsetLeft - posX2) + "px";
+
+        if (Math.abs(posY2) > 10) {
+            noshift = true;
+            posX1 = 0;
+            posX2 = 0;
+            return false;
+        }
+
+        if (Math.abs(posX2) > 10) {
+            items.style.left = (parseInt(items.style.left.replace('px')) - parseInt(posX2)) + "px";
+            if (e.type == 'touchmove') {
+                posX1 = e.touches[0].clientX;
+                posY1 = e.touches[0].clientY;
+                
+            } else {
+                posX1 = e.clientX;
+                posY1 = e.clientY;
+            }
+            return false;
+        } 
     }
 
     function dragEnd(e) {
@@ -180,6 +253,16 @@ function madSlider(wrapper) {
 
         document.onmouseup = null;
         document.onmousemove = null;
+
+        var is = items.querySelectorAll('a');
+        is.forEach(function(i) {
+            i.style.pointerEvents = '';
+        });
+
+        items.classList.remove('dragging');
+
+        noshift = false;
+        return false;
     }
 
     function shiftSlide(dir, action) {
@@ -189,20 +272,20 @@ function madSlider(wrapper) {
             if (!action) { posInitial = items.offsetLeft; }
             if (!loop) {
                 if (index + dir > slidesLength - slidesPerView) {
-                    dir = 0; index = slidesLength - slidesPerView; posInitial = -slideSize * index;
+                    dir = 0; index = slidesLength - slidesPerView; posInitial = -(slideSize + gap) * index;
                 }
                 if (index + dir < 0) { 
                     dir = 0; index = 0; posInitial = 0;
                 }
             } 
-            items.style.left = (posInitial - (slideSize * dir)) + "px";
+            items.style.left = (posInitial - ((slideSize+ gap) * dir )) + "px";
             index += dir;
         };
 
         allowShift = dir == 0 ? true : false;
 
-        if (pagination) {
-            Array.from(pagination.getElementsByClassName('mad-slider-button')).forEach(element => {
+        if (paginationItems) {
+            paginationItems.forEach(element => {
                 element.classList.remove('active');
                 if (element.dataset.index == index % slidesLength || (index == -1 && element.dataset.index == slidesLength - 1)) {
                     element.classList.add('active');
@@ -218,10 +301,19 @@ function madSlider(wrapper) {
         items.classList.add('shifting');
 
         if (allowShift && n != index) {
-            items.style.left = (-(slideSize * (n + slidesShift))) + "px";
+            items.style.left = (-((slideSize + gap) * (n + slidesShift))) + "px";
             index = n;
             allowShift = false;
         };
+
+        if (paginationItems) {
+            paginationItems.forEach(element => {
+                element.classList.remove('active');
+                if (element.dataset.index == index % slidesLength || (index == -1 && element.dataset.index == slidesLength - 1)) {
+                    element.classList.add('active');
+                }
+            });
+        }
     }
 
     function checkIndex() {
@@ -229,13 +321,23 @@ function madSlider(wrapper) {
 
         if (index < 0) {
             index = slidesShift + index;
-            items.style.left = -((index + slidesShift) * slideSize) + "px";
+            items.style.left = -((index + slidesShift) * (slideSize + gap)) + "px";
 
         } else if (index > slidesLength - 1) {
             index = index % slidesLength;
-            items.style.left = -((index + slidesShift) * slideSize) + "px";
+            items.style.left = -((index + slidesShift) * (slideSize + gap)) + "px";
         }
-        items.style.height = slides[index].clientHeight + "px";
+
+        index = parseInt(index);
+
+        if (autoheight && slides.length) { 
+            var h = slides[index].clientHeight;
+
+            for (i = 1; i < slidesPerView; i++) {
+                if (h < slides[index + i].clientHeight) h = slides[index + i].clientHeight;
+            }
+            items.style.height = h + "px";
+        }
 
         allowShift = true;
 
